4a917e3aaf52469830c51f42790a582601114ab6
Muhammad Khattab <msskzx@gmail.com>
2023-04-03 19:25:14 +0200

    finished assignment 1
    this includes:
    - data loading, understanding, manipulation

--- Assignment1.R
+++ Assignment1.R
@@ -1,11 +1,19 @@
-# install.packages("dslabs") # execute only once!
-library(dslabs) # execute in every new script
+# execute only once!
+# install.packages("dslabs") 
+# execute in every new script
+library(dslabs)
+
+
 
 # 1. What is the sum of the first 100 positive integers?
-# The formula for the sum of integers from 1 to n is n(n + 1)/2.
-# Define n = 100 and then use R to compute the sum from 1 to 100 using the formula.
-# What is the value of the sum?
-
 n <- 100
 result <- n*(n+1)/2
 result
+
+# 3. Use seq function instead
+n <- 1000
+required_sequence <- seq(1:n)
+sum(required_sequence)
+
+# 4. Use one line of code to compute the logarithm, in base 10, of the square root of 100.
+log10(sqrt(100))
